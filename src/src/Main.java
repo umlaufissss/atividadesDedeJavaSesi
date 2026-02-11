c4349a6ffdb7c7b7cdc90e6218898d2cc779155c
umlaufissss <umlaufisadora@gmail.com>
2026-02-11 16:32:06 -0300

    Feat: atividade 1 + alteraçcao de Main

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+
 public class Main
 {
+    static Scanner SC = new Scanner(System.in);
+
     public static void main(String[] args)
     {
+       inicio();
+    }
+
+    public static void inicio()
+    {
         System.out.println("Atividades do professor Dedé: \nAula - Entrada de Dados (Scanner)");
+
+        System.out.println("Digite qual questão você gostaria de entrar:");
+        System.out.println("[1]                     [5]");
+        System.out.println("[2]                     [6]");
+        System.out.println("[3]                     [7]");
+        System.out.println("[4]                     [8]");
+        System.out.println("[9] - SAIR");
+
+        int escolha = SC.nextInt();
+        SC.nextLine();
+
+        switch (escolha)
+        {
+            case 1:
+                atv1.atividade1();
+        }
     }
 }
--- src/src/atv1.java
+++ src/src/atv1.java
@@ -1,2 +1,23 @@
-public class atv1 {
+import java.util.*;
+
+public class atv1 extends Main
+{
+    //COMANDO: Peça ao usuário seu nome e exiba uma mensagem de saudação
+    //personalizada.
+    public static void atividade1()
+    {
+        String nome;
+
+        System.out.println("Olá, digite o seu nome:");
+        nome = SC.nextLine();
+
+        System.out.println("Seja bem-vindo, " + nome + "\n");
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        inicio();
+    }
 }
