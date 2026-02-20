27d81303fd7a96261895b62a634c136037b20418
umlaufissss <umlaufisadora@gmail.com>
2026-02-20 20:55:26 -0300

    Style: Estilizalção da Main

--- AtividadeVetor/src/Main.java
+++ AtividadeVetor/src/Main.java
@@ -13,6 +13,34 @@ public class Main
 
     public static void inicio()
     {
+        System.out.println("Atividades do professor Dedé: \nAula - Entrada de Dados (Scanner)");
 
+        System.out.println("▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀");
+        System.out.println("┌────────────────────────────────────────────┐");
+        System.out.println("│Digite qual questão você gostaria de entrar:│");
+        System.out.println("├────────────────────────────────────────────┤");
+        System.out.println("│[1]                     [3]                 │");
+        System.out.println("│[2]                     [4]                 │");
+        System.out.println("├────────────────────────────────────────────┤");
+        System.out.println("│[9] - SAIR                                  │");
+        System.out.println("└────────────────────────────────────────────┘");
+
+        int escolha = SC.nextInt();
+        SC.nextLine();
+
+        switch (escolha)
+        {
+            case 1 ->atv1.atividade1();
+            case 2 ->atv2.atividade2();
+            case 3 ->atv3.atividade3();
+            case 4 ->atv4.atividade4();
+            case 9-> System.exit(0);
+            default->
+            {
+                System.out.println("Opção inválida!");
+                inicio();
+                break;
+            }
+        }
     }
 }
