ba8f0fea72137018b416b6582230c48b30b05ff0
umlaufissss <umlaufisadora@gmail.com>
2026-02-20 18:57:11 -0300

    Feat: Atividade 2

--- AtividadeVetor/src/Main.java
+++ AtividadeVetor/src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main
+{
+    static Scanner SC = new Scanner(System.in);
+
     public static void main(String[] args)
     {
         inicio();
---
+++ AtividadeVetor/src/atv2.java
@@ -0,0 +1,30 @@
+public class atv2 extends Main
+{
+    public static void atividade2()
+    {
+        int[] numeros = new int[5];
+        int num = 0;
+
+        System.out.println("Digite seus números: ");
+
+        for(int i = 0; i<5;i++)
+        {
+            System.out.println("Número " + (i+1) + ":");
+            numeros[i] = SC.nextInt();
+            num += numeros[i];
+        }
+
+        System.out.println("A soma de todos os números é: " + num);
+
+        try
+        {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+        }
+
+        inicio();
+    }
+}
