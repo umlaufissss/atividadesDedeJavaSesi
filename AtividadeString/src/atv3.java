d58dc5fac6e5c4ab15e4d337c52815fc634de8c5
umlaufissss <umlaufisadora@gmail.com>
2026-02-23 18:28:57 -0300

    feat: activity 3's creation

--- AtividadeString/src/atv3.java
+++ AtividadeString/src/atv3.java
@@ -1,4 +1,41 @@
-public class atv3
+public class atv3 extends Main
 {
+    public static void atividade3()
+    {
+        boolean ehValido = false;
+        int arroba = 0;
 
+        System.out.println("Digite o seu e-mail");
+        String emailValido = SC.nextLine();
+
+        if(emailValido.contains("@") && emailValido.contains("."))
+        {
+            if(!emailValido.startsWith("@"))
+            {
+                ehValido = true;
+                arroba = emailValido.indexOf("@");
+            }
+        }
+
+        if(ehValido)
+        {
+            System.out.println(emailValido.substring(0, arroba - 1));
+            System.out.println(emailValido.substring(arroba));
+        }
+        else
+        {
+            System.out.println("E-mail inválido!");
+        }
+
+        try
+        {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+        }
+
+        inicio();
+    }
 }
