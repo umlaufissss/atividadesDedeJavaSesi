fc10ae8c7303e3e73f2e05b0e8893432179e6b8e
umlaufissss <umlaufisadora@gmail.com>
2026-02-26 10:18:28 -0300

    feat: criação de atividade 5

---
+++ AtividadeArrayList/src/atv5.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class atv5 extends Main
+{
+    public static void atividade5()
+    {
+        List<String> listPalavra = new ArrayList<>();
+
+        for(int i = 0; i<5;i++)
+        {
+            System.out.println("Digite palavra " + i+1+ ": ");
+            String palavra = SC.nextLine();
+            listPalavra.add(palavra);
+        }
+
+        System.out.println("Suas inserções na ordem inversa: ");
+        for(int i = 5; i > 0; i--)
+        {
+            System.out.println(listPalavra.get(i));
+        }
+
+        try
+        {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+        }
+
+        inicio();
+    }
+}
