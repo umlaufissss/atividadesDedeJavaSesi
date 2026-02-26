95b3a0b6e3b4acbccdd28a53f96e16891e699e64
umlaufissss <umlaufisadora@gmail.com>
2026-02-26 10:20:01 -0300

    style: decoração de main

--- AtividadeArrayList/src/Main.java
+++ AtividadeArrayList/src/Main.java
@@ -11,6 +11,35 @@ public class Main
 
     public static void inicio()
     {
+        System.out.println("Atividades do professor Dedé: \nAula - Entrada de Dados (Scanner)");
 
+        System.out.println("▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀");
+        System.out.println("┌────────────────────────────────────────────┐");
+        System.out.println("│Digite qual questão você gostaria de entrar:│");
+        System.out.println("├────────────────────────────────────────────┤");
+        System.out.println("│[1]                     [2]                 │");
+        System.out.println("│[3]                     [4]                 │");
+        System.out.println("│[5]                                         │");
+        System.out.println("├────────────────────────────────────────────┤");
+        System.out.println("│[0] - SAIR                                  │");
+        System.out.println("└────────────────────────────────────────────┘");
+
+        int escolha = SC.nextInt();
+        SC.nextLine();
+
+        switch (escolha)
+        {
+            case 1 ->atv1.atividade1();
+            case 2 ->atv2.atividade2();
+            case 3 ->atv3.atividade3();
+            case 4 ->atv4.atividade4();
+            case 5 ->atv5.atividade5();
+            case 0-> System.exit(0);
+            default->
+            {
+                System.out.println("Opção inválida!");
+                inicio();
+            }
+        }
     }
 }
