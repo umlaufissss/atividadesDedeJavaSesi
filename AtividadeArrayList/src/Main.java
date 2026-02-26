d636f02f7c351a67a414f6c260fd4433cee6fe5c
umlaufissss <umlaufisadora@gmail.com>
2026-02-26 09:55:41 -0300

    feat: criação de atividade 1 e Main

---
+++ AtividadeArrayList/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main
+{
+    static Scanner SC = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        inicio();
+    }
+
+    public static void inicio()
+    {
+
+    }
+}
---
+++ AtividadeArrayList/src/atv1.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class atv1 extends Main
+{
+    public static void atividade1()
+    {
+        List<String> listaNome = new ArrayList<>();
+
+        for(int i =0; i<5;i++)
+        {
+            System.out.println("Digite o nome " +i+1);
+            String nome = SC.nextLine();
+            listaNome.add(nome);
+        }
+
+        for(String nome : listaNome)
+        {
+            System.out.println(nome);
+        }
+
+        try
+        {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+        }
+
+        inicio();
+    }
+}
