5bd920e1d0f5bcbe2521eea96278f78d7d175467
umlaufissss <umlaufisadora@gmail.com>
2026-03-02 19:58:57 -0300

    feat: model 1 - Espada

---
+++ AtividadePOO/src/atv1Espada.java
@@ -0,0 +1,16 @@
+public class atv1Espada
+{
+    public String nome;
+    public int dano;
+    public String material;
+
+    public void atacar()
+    {
+        System.out.println("A " + nome + " está atacando...");
+    }
+
+    public void afiar()
+    {
+        System.out.println("A " + nome + " está sendo afiada...");
+    }
+}
