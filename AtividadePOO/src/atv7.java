d6b5e7d5c60047b4d1033b1755880484d7c19365
umlaufissss <umlaufisadora@gmail.com>
2026-03-02 21:10:35 -0300

    feat: criacao de main de atividade 7

--- AtividadePOO/src/atv7.java
+++ AtividadePOO/src/atv7.java
@@ -1,7 +1,43 @@
-public class atv7
+public class atv7 extends Main
 {
     public static void atividade7()
     {
+        Carro carro = new Carro();
+        carro.marca = "Mustang";
+        carro.cor = "Preto";
+        carro.velocidadeAtual = 200;
 
+        System.out.println("Digite o que você quer fazer: ");
+        System.out.println("[1] - Acelerar || [2] - Freiar || [3] - Mostrar velocidade atual");
+        int escolha = SC.nextInt();
+
+        if(escolha == 1)
+        {
+            carro.acelerar();
+        }
+        else if(escolha == 2)
+        {
+            carro.freiar();
+        }
+        else if(escolha == 3)
+        {
+            carro.mostrarVelocidade();
+        }
+        else
+        {
+            System.out.println("OPÇÃO INVÁLIDA");
+            atividade7();
+        }
+
+        try
+        {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+        }
+
+        inicio();
     }
 }
