1376e0a217ad3d496a2be76d01ff94c12eb0b52d
umlaufissss <umlaufisadora@gmail.com>
2026-03-09 15:16:46 -0300

    feat: Criação de classe ContaBancaria

---
+++ AtividadePOO/AtividadePOOEncapsulamento/src/main/java/org/example/ContaBancaria.java
@@ -0,0 +1,52 @@
+package org.example;
+
+public class ContaBancaria
+{
+    private String titular;
+    private double saldo;
+
+    public ContaBancaria(String titular, double saldo) {
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void depositar (double valor)
+    {
+        System.out.println("Adicionando a conta...");
+        setSaldo(getSaldo()+valor);
+    }
+
+    public void sacar (double valor)
+    {
+        if(getSaldo() - valor <0)
+        {
+            System.out.println("ERRO! O SALDO NÃO PODE ESTAR NEGATIVO");
+        }
+        else
+        {
+            System.out.println("Sacando da conta...");
+            setSaldo(getSaldo()-valor);
+        }
+    }
+
+    public void mostrarSaldo()
+    {
+        System.out.println("Olá, " + getTitular() + "! \nSeu saldo é: R$" + getSaldo());
+    }
+}
