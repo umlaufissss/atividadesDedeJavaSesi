94c7736790b02ac7fb6d38a4ac514c5f885c6191
umlaufissss <umlaufisadora@gmail.com>
2026-03-09 15:25:16 -0300

    feat: Criação da classe Carro

---
+++ AtividadePOO/AtividadePOOEncapsulamento/src/main/java/org/example/Carro.java
@@ -0,0 +1,63 @@
+package org.example;
+
+public class Carro
+{
+    private String modelo;
+    private int ano;
+    private double velocidade;
+
+    public Carro(String modelo, int ano, double velocidade) {
+        this.modelo = modelo;
+        this.ano = ano;
+        this.velocidade = velocidade;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(double velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public void acelerar(double valor)
+    {
+        System.out.println("Acelerando...");
+        setVelocidade(getVelocidade()+valor);
+    }
+
+    public void freiar(double valor)
+    {
+        if(getVelocidade() - valor <0)
+        {
+            System.out.println("ERRO! O Carro já está parado a esse ponto");
+            setVelocidade(0);
+        }
+        else
+        {
+            System.out.println("Freiando...");
+            setVelocidade(getVelocidade() - valor);
+        }
+    }
+
+    public void mostrarVelocidade()
+    {
+        System.out.println("VELODIDADE = " + getVelocidade() + "Km/H");
+    }
+}
