c3fba7b813ff84a2a1898d75fb98d80a49174706
umlaufissss <umlaufisadora@gmail.com>
2026-03-09 21:22:17 -0300

    style: arrumar detalhes da main

--- AtividadePOO/AtividadePOOEncapsulamento/src/main/java/org/example/Main.java
+++ AtividadePOO/AtividadePOOEncapsulamento/src/main/java/org/example/Main.java
@@ -21,12 +21,11 @@ public class Main {
             System.out.println("┌────────────────────────────────────────────┐");
             System.out.println("│Digite qual questão você gostaria de entrar:│");
             System.out.println("├────────────────────────────────────────────┤");
-            System.out.println("│[1]                     [5]                 │");
-            System.out.println("│[2]                     [6]                 │");
-            System.out.println("│[3]                     [7]                 │");
-            System.out.println("│[4]                     [8]                 │");
+            System.out.println("│[1]                     [2]                 │");
+            System.out.println("│[3]                     [4]                 │");
+            System.out.println("│[5]                                         │");
             System.out.println("├────────────────────────────────────────────┤");
-            System.out.println("│[10] - SAIR                                 │");
+            System.out.println("│[0] - SAIR                                 │");
             System.out.println("└────────────────────────────────────────────┘");
 
             int escolha = SC.nextInt();
@@ -38,10 +37,7 @@ public class Main {
                 case 3 -> atv3.atividade3();
                 case 4 -> atv4.atividade4();
                 case 5 -> atv5.atividade5();
-                case 6 -> atv6.atividade6();
-                case 7 -> atv7.atividade7();
-                case 8 -> atv8.atividade8();
-                case 10 -> System.exit(0);
+                case 0 -> System.exit(0);
                 default -> {
                     System.out.println("Opção inválida!");
                     inicio();
